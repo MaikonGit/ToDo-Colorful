9eac43e42098a46da3e4387d94d66d2d2d02f23e
Maikon <maikon.ferreirayt@gmail.com>
2022-01-03 06:36:35 -0300

    tableView Found nil ??

--- ToDo- Colorful/MODEL/ToDo Brain.swift
+++ ToDo- Colorful/MODEL/ToDo Brain.swift
@@ -9,35 +9,35 @@ import UIKit
 import CoreData
 
 class TodoBrain {
+    
+    
     //shared Constant
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
-    //custom array da tableView
-    var itemArray = [Item]()
+  
     
     
+   
     
-    //MARK: - SAVE DATA FUNC
-    func saveData() {
-       
-        do {
-           try self.context.save()
-        } catch {
-            print("Error saving context: \(error)")        }
-        
-        
-    }
-    
-    //MARK: - LOAD DATA FUNC
-    func loadData(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
-        
-        do {
-          itemArray = try context.fetch(request)
-        } catch {
-            print("Error fetching data from context: \(error)")
-        }
-        
-    }
-    
-}
+//    //MARK: - SAVE DATA FUNC
+//    func saveData() {
+//        do {
+//            try self.context.save()
+//        } catch {
+//            print("Error saving context: \(error)")
+//        }
+//    }
+//    
+//    //MARK: - LOAD DATA FUNC
+//    func loadData(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+//        
+//        let predicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectCategory!.name!)
+//        request.predicate = predicate
+//        
+//        do {
+//            itemArray = try context.fetch(request)
+//        } catch {
+//            print("Error fetching data from context: \(error)")
+//        }
+//    }
+}//class
 
--- ToDo- Colorful/Supporting Files/Constants.swift
+++ ToDo- Colorful/Supporting Files/Constants.swift
@@ -10,6 +10,12 @@ import Foundation
 struct K {
     //Cell Identifiers
     static let cellIdentifier1 = "TodoItemCell"
+    static let cellIdentifier2 = "CategoryCell"
+    
+    //nibNames
     static let nibName = "TodoTableViewCell"
+    static let nibName2 = "CategoryTableViewCell"
+    
+    //persistent container
     static let persistentContainer = "TodoDataModel"
 }
